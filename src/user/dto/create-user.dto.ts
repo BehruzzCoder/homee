df18ea2418f35ff2d8d9af1e23b95f3384403908
BehruzzCoder <BehruzzCoder@gmail.com>
2025-05-15 15:50:13 +0500

    swagger

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,8 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { UserRole } from "@prisma/client";
 
 export class CreateUserDto {
+    @ApiProperty()
     name: string;
     password: string;
+    @ApiProperty()
     email: string;
+    @ApiProperty()
     role: UserRole
 }
